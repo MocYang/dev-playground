33122532c0e06957788a4163b4a4085de759c6a8
MocYang <moclinging@gmail.com>
2018-03-02 18:40:10 +0800

    add vuex template

---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="counter">
+      <div class="counter__number">{{count}}</div>
+      <button class="btn__add">add</button>
+      <button class="btn__sub">sub</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
