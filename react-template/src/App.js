66eeac76f337dc169825ea7fa9a5aecab5371a27
MocYang <moclinging@gmail.com>
2018-06-27 15:24:33 +0800

    feat(finish template files.):

--- react-template/src/App.js
+++ react-template/src/App.js
@@ -1,21 +1,51 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+/* eslint-disable-next-line */
+import React, { Component } from 'react'
 import './assets/styles/style.css'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.page = React.createRef()
+    this.state = {
+      pageHeight: 0,
+      emojiScale: 1
+    }
+  }
+
+  componentDidMount () {
+    const pageElem = this.page.current
+    this.setState({
+      pageHeight: pageElem.offsetHeight
+    })
+  }
+
+  handleScroll = (e) => {
+    const appElem = e.target
+    const pageHeight = this.state.pageHeight
+    const scrollTop = appElem.scrollTop
+    const activePage = scrollTop / pageHeight
+    // 缩放倍率需要根据元素实际位置调整
+    if (activePage >= 1 && activePage <= 2.3) {
+      this.setState({
+        emojiScale: 4 * (1 - (activePage - 1.3))
+      })
+    }
+  }
+
   render () {
+    const emojiScale = this.state.emojiScale
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app" onScroll={this.handleScroll}>
+        <div ref={this.page} className="page page1" />
+        <div className="page page2" />
+        <div className="page page3">
+          <div className="page3__bg" />
+          <div className="emoji-mdr" style={{transform: `rotateX(-30deg) scale(${emojiScale})`}}/>
+        </div>
+        <div className="page page4" />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
