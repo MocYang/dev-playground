3fdc1a47742d7d26097312ef8f42e681c07af68a
MocYang <moclinging@gmail.com>
2018-07-24 09:34:56 +0800

    feat: add control flow.

---
+++ python-v3/src/3-control-flow/control-flow.py
@@ -0,0 +1,21 @@
+# -*- encoding: UTF-8 -*-
+
+# 流程控制语句
+# 1 if
+
+# x = input('输入数字：')
+# if int(x) > 10:
+#     print(x + '大于10')
+# elif int(x) < 10:
+#     print(x + '小于10')
+# else:
+#     print(x + '等于10')
+
+# 2 for
+words = ['a', 'b', 'c']
+people = [{'name': '1'}, {'name': '2'}]
+for w in words:
+    print(w)
+
+for p in people:
+    print(p['name'])
