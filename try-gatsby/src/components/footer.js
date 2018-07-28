19bfc67c7083ded3b4d9948dfe161fc8ba9b73c7
杨启信 <yangqixin@yangqixin.local>
2018-07-28 22:50:26 +0800

    remove problem packges

--- try-gatsby/src/components/footer.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-const Footer = () => (
-  <div className="footer">
-    no thing here yet.
-  </div>
-)
-
-export default Footer
--- try-gatsby/src/pages/page-3.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable-next-line */
-import React, { Component } from 'react'
-const Page3 = () => (
-  <div className="page3">
-    <p>page 3</p>
-  </div>
-)
-
-export default Page3
