1c888e362f56aa5c55255e167a61584043bd7feb
MocYang <moclinging@gmail.com>
2018-11-27 18:40:15 +0800

    feat: add dict test.

--- python-v3/2-learning-python-E5/chapter-8-list-and-dictionary/dictionary.py
+++ python-v3/2-learning-python-E5/chapter-8-list-and-dictionary/dictionary.py
@@ -1,1 +1,16 @@
 # -*- charset: utf-8 -*-
+
+d1 = {'name': 1, 'age': 2}
+print(d1['name'])
+print(d1['age'])
+print(d1.keys())                # dict_keys(['name', 'age'])
+print(tuple(d1.keys()))         # ('name', 'age')
+print(list(d1.keys()))          # ['name', 'age']
+print(d1.items())
+print(tuple(d1.items()))        # (('name', 1), ('age', 2))
+print(list(d1.items()))         # [('name', 1), ('age', 2)]
+
+dt1 = tuple(d1.items())
+print(dt1[0][0], dt1[0][1])
+
+
