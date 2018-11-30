9f403a812614f3725dba9eb6b3b21a00df39ea66
MocYang <moclinging@gmail.com>
2018-11-30 13:43:57 +0800

    chore: remove unuse files.

--- vue-demos/_assets.php
+++
@@ -1,7 +0,0 @@
-<?php
-  return [
-    'vendor' => '<%= htmlWebpackPlugin.files.chunks._vendor.entry %>',
-
-    'app_css' => '<%= htmlWebpackPlugin.files.chunks.app.css %>',
-    'app_js' => '<%= htmlWebpackPlugin.files.chunks.app.entry %>'
-  ];
