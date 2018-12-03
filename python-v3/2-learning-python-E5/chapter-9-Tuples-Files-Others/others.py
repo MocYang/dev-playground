8a67825cff3f7e2c17ef1bb6a9f64f48b3ba6e6f
MocYang <moclinging@gmail.com>
2018-12-03 11:56:38 +0800

    feat: finish chapter9

--- python-v3/2-learning-python-E5/chapter-9-Tuples-Files-Others/others.py
+++ python-v3/2-learning-python-E5/chapter-9-Tuples-Files-Others/others.py
@@ -4,24 +4,24 @@
 # ==    tests value equivalence recursively.
 # is    test object identity.
 
-L1 = [1, ('a', 3)]
-L2 = [1, ('a', 3)]
-
-print(L1 == L2, L1 is L2)   # True False
-L3 = L1
-print(L3 == L1, L3 == L2, L1 is L3)  # True True True
+# L1 = [1, ('a', 3)]
+# L2 = [1, ('a', 3)]
+#
+# print(L1 == L2, L1 is L2)   # True False
+# L3 = L1
+# print(L3 == L1, L3 == L2, L1 is L3)  # True True True
 
 # for str, number
-S1 = 'spam'
-S2 = 'spam'
+# S1 = 'spam'
+# S2 = 'spam'
 
 # short str were caches by python and reuse, so ==, is is the same
-print(S1 == S2, S1 is S2)  # True True
+# print(S1 == S2, S1 is S2)  # True True
 
-# for long str
-S3 = 'i am long string aaaaaaasdfads asdfasdfasdfads'
-S4 = 'i am long string aaaaaaasdfads asdfasdfasdfads'
-print(S3 == S4, S3 is S4)
+# for long str （test fail for now）
+# S3 = 'i am long string aaaaaaasdfads asdfasdfasdfads'
+# S4 = 'i am long string aaaaaaasdfads asdfasdfasdfads'
+# print(S3 == S4, S3 is S4)
 
 # True and False
 # Numbers are False if zero, and True otherwise
@@ -39,7 +39,8 @@ print(S3 == S4, S3 is S4)
 # None              False
 
 
-
+# type(X)
+# return type of X
 
 
 
