c9831e6691002418f4263f556e3dba725545317f
MocYang <moclinging@gmail.com>
2018-12-17 13:35:26 +0800

    chore:

--- python-v3/2-learning-python-E5/chapter-10-To-13-Statements/13-while-and-for-loop.py
+++ python-v3/2-learning-python-E5/chapter-10-To-13-Statements/13-while-and-for-loop.py
@@ -6,6 +6,16 @@
 # else:                 # optional else
 #       statements      # Run if did't exit loop with break
 
-
+# y = 185
+# x = y // 2
+# while x > 1:
+#     if y % x == 0:
+#         print(y, 'has factor ', x)
+#         break
+#     x -= 1
+#     print(x)
+#
+# else:
+#     print(y, 'is prime')
 
 
