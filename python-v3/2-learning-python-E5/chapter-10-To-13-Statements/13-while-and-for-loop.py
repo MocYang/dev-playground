8caa2f44610e4ff6c3f0d59ce5b38d21335ccf08
MocYang <moclinging@gmail.com>
2019-03-06 14:42:47 +0800

    feat: update onto chapter16-function

--- python-v3/2-learning-python-E5/chapter-10-To-13-Statements/13-while-and-for-loop.py
+++ python-v3/2-learning-python-E5/chapter-10-To-13-Statements/13-while-and-for-loop.py
@@ -19,33 +19,3 @@
 #     print(y, 'is prime')
 
 
-# for item in sequence:
-#      do something
-# else:
-#       runs without break.
-
-#
-# L = [1, 2, 3, 4, 5]
-# for i in range(len(L)):
-#     print(L[i:] + L[:i])
-
-
-# methods
-# 1 list(), range(), len()
-
-# 2 zip(), map()
-# keys = ['a', 'b', 'c']
-# values = [1, 2, 3]
-# print(list(zip(keys, values)))  # [('a', 1), ('b', 2), ('c', 3)]
-# print({k: v for (k, v) in zip(keys, values)})  # {'a': 1, 'b': 2, 'c': 3}
-# print(dict(zip(keys, values)))  # {'a': 1, 'b': 2, 'c': 3}
-# print(list(map(ord, 'abcd')))  # [97, 98, 99, 100]
-
-# Generating Both Offsets and Items: enumerate
-
-# S = 'abcdef'
-# for (offset, char) in enumerate(S):
-#     print(offset, '---', char)
-
-
-
--- python-v3/2-learning-python-E5/chapter-10-To-13-Statements/script.py
+++
@@ -1,8 +0,0 @@
-# -*- charset: utf-8 -*-
-import sys
-
-print(sys.path)
-x = 2
-print(x **2 )
-
-
