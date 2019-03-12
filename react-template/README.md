36b93c54b84dbfa2e85de83fa3baef749a47beda
MocYang <moclinging@gmail.com>
2019-03-12 10:31:16 +0800

    feat: remove react-template

--- react-template/README.md
+++
@@ -1,3 +0,0 @@
-# issues
-- 1 [ ] Carousel 点击翻页时，ProgressBar没有重置
-- 2 [ ] Hooks 整合Redux
--- react-template/src/components/Indicator.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Indicator = (props) => <div className={`indicator ${props.active ? 'active' : ''}`} onClick={props.onClick}/>
-
-export default Indicator
