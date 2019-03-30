653035affaa5493863526f6a86e859c58233ee43
MocYang <moclinging@gmail.com>
2019-03-30 22:03:00 +0800

    feat: add model users

--- py_root/py_root/settings.py
+++ py_root/py_root/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'react_admin_python.apps.ReactAdminPythonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- py_root/react_admin_python/models.py
+++ py_root/react_admin_python/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=40)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField('time created')
+    updated_at = models.DateTimeField('time updated')
+
+    def __str__(self):
+        return self.name
+
