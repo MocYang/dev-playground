e623d76f9d6d2b7813f30f43001dcc4808f50eff
Qixin Yang <moclinging@gmail.com>
2019-04-07 21:38:55 +0800

    feat: 完成全排列算法

--- DA-master/src/算法/全排列.js
+++ DA-master/src/算法/全排列.js
@@ -3,21 +3,51 @@
  * 求给定序列的全排列
  */
 
- const fixedList = [1, 2, 3]
+const fixedList = [1, 2, 3]
 
- const fullArrange = (list) => {
-   const length = list.length
-
-   for (let i = 0; i < length; i++) {
-     for (let j = 0; j < length - i; j++) {
+// 求给定序列的所有可能的组合
+const fullArrange = (list) => {
+  const length = list.length
+  let ret = []
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - i; j++) {
       let sequence = []
       for (let k = j; k <= (j + i); k++) {
         sequence.push(list[k])
       }
-      console.log(sequence)
-     }
-   }
- }
+      // console.log(sequence)
+      ret.push(permute(sequence))
+    }
+  }
+  return ret
+}
 
- fullArrange(fixedList)
+// 求给定序列的全排列
+const permute = (list) => {
+  if (list.length <= 1) {
+    return list
+  }
+  let ret = []
+  for (let i = 0; i < list.length; i++) {
+    let sub = list.slice(0, i).concat(list.slice(i + 1))
+    let subPermute = permute(sub)
+    for (let x of subPermute) {
+      ret.push(list.slice(i, i + 1).concat(x))
+    }
+  }
+  return ret
+}
+
+console.log(fullArrange(fixedList))
+
+// console.log(permute([1, 2, 3]))
+/*
+  [ [ 1, 2, 3 ],
+  [ 1, 3, 2 ],
+  [ 2, 1, 3 ],
+  [ 2, 3, 1 ],
+  [ 3, 1, 2 ],
+  [ 3, 2, 1 ] ]
+ */
+
 
--- python-v3/2-learning-python-E5/chapter20-comprehensions-and-generations/index.py
+++ python-v3/2-learning-python-E5/chapter20-comprehensions-and-generations/index.py
@@ -18,3 +18,17 @@ def gensquares(n):
 # print(gen1.__next__())
 # print(gen1.__next__())
 # print(gen1.__next__())
+
+def permute1(seq):
+    if not seq:
+        return [seq]
+    ret = []
+    for i in range(len(seq)):
+        rest = seq[:i] + seq[i+1:]
+        for x in permute1(rest):
+            ret.append(seq[i:i+1] + x)
+
+    return ret
+
+
+print(permute1([1, 2, 3]))
